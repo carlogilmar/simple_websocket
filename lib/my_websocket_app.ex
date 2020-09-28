7c02a57c4cd5ec3530ee27ff74cd4298fbe3c58b
Carlo Gilmar <carlogilmar12@gmail.com>
2020-09-27 21:36:34 -0500

    Adding basic websocket

---
+++ lib/my_websocket_app.ex
@@ -0,0 +1,18 @@
+defmodule MyWebsocketApp do
+  @moduledoc """
+  Documentation for `MyWebsocketApp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MyWebsocketApp.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/my_websocket_app/application.ex
@@ -0,0 +1,42 @@
+defmodule MyWebsocketApp.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: MyWebsocketApp.Worker.start_link(arg)
+      # {MyWebsocketApp.Worker, arg}
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: MyWebsocketApp.Router,
+        options: [
+          dispatch: dispatch(),
+          port: 4000
+        ]
+      ),
+      Registry.child_spec(
+        keys: :duplicate,
+        name: Registry.MyWebsocketApp
+      )
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: MyWebsocketApp.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defp dispatch do
+    [
+      {:_,
+        [
+          {"/ws/[...]", MyWebsocketApp.SocketHandler, []},
+          {:_, Plug.Cowboy.Handler, {MyWebsocketApp.Router, []}}
+        ]
+      }
+    ]
+  end
+end
